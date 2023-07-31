845b6702a9b958a046cb35090e6dbd85a6b4ac79
gioboa <giorgiob.boa@gmail.com>
2023-07-31 10:14:33 +0200

    remove @vue-storefront/sdk

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,29 +1,8 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
-import { sdk } from '~/sdk';
-
 
 export default component$(() => {
-	return (
-		<>
-			<h1>Hi 👋</h1>
-			<p>
-				Can't wait to see what you build with qwik!
-				<br />
-				Happy coding.
-				<button
-					onClick$={async () => {
-						const abc = await sdk.commerce.getContent<any>({
-							url: 'home-page',
-						});
-						console.log(abc);
-					}}
-				>
-					HomePage
-				</button>
-			</p>
-		</>
-	);
+	return <>Hello!</>;
 });
 
 export const head: DocumentHead = {
--- src/sdk/index.ts
+++
@@ -1,8 +0,0 @@
-import { initSDK, buildModule } from '@vue-storefront/sdk';
-import { type SdkModule, sdkModule } from '@vue-storefront/storefront-boilerplate-sdk';
-
-const sdkConfig = {
-  commerce: buildModule<SdkModule>(sdkModule),
-};
-
-export const sdk = initSDK<typeof sdkConfig>(sdkConfig);
