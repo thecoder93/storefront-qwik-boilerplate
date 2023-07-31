4dd8afa313cb7ab4b32c73cf4151d504739354ac
gioboa <giorgiob.boa@gmail.com>
2023-07-31 15:59:10 +0200

    add aria-label

--- src/components/ProductSlider/ProductSlider.tsx
+++ src/components/ProductSlider/ProductSlider.tsx
@@ -38,6 +38,7 @@ export const ProductSlider = component$<ProductSliderProps>(({ products }) => {
 		<div class='items-center relative flex max-w-screen-3xl mx-auto px-4 md:px-10 mb-20'>
 			<button
 				type='button'
+				aria-label='prev'
 				class='inline-flex items-center justify-center font-medium text-base focus-visible:outline focus-visible:outline-offset rounded-md disabled:text-disabled-500 disabled:bg-disabled-300 disabled:shadow-none disabled:ring-0 disabled:cursor-not-allowed p-4 gap-3 text-primary-700 hover:bg-primary-100 hover:text-primary-800 active:bg-primary-200 active:text-primary-900 ring-1 ring-primary-700 hover:shadow-md active:shadow shadow hover:ring-primary-800 active:ring-primary-900 disabled:ring-1 disabled:ring-disabled-300 disabled:bg-white/50 hidden md:block !ring-neutral-500 !text-neutral-500 disabled:!ring-disabled-300 disabled:!text-disabled-500 !rounded-full bg-white absolute left-4 z-10'
 				data-testid='button'
 			>
@@ -70,6 +71,7 @@ export const ProductSlider = component$<ProductSliderProps>(({ products }) => {
 			</div>
 			<button
 				type='button'
+				aria-label='next'
 				class='inline-flex items-center justify-center font-medium text-base focus-visible:outline focus-visible:outline-offset rounded-md disabled:text-disabled-500 disabled:bg-disabled-300 disabled:shadow-none disabled:ring-0 disabled:cursor-not-allowed p-4 gap-3 text-primary-700 hover:bg-primary-100 hover:text-primary-800 active:bg-primary-200 active:text-primary-900 ring-1 ring-primary-700 hover:shadow-md active:shadow shadow hover:ring-primary-800 active:ring-primary-900 disabled:ring-1 disabled:ring-disabled-300 disabled:bg-white/50 hidden md:block !ring-neutral-500 !text-neutral-500 disabled:!ring-disabled-300 disabled:!text-disabled-500 !rounded-full bg-white absolute right-4 z-10'
 				data-testid='button'
 			>
