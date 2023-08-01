a119dc5d95e216c7b63c5260f3db17c034e22520
gioboa <giorgiob.boa@gmail.com>
2023-08-01 20:49:07 +0200

    add missing label

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -99,7 +99,7 @@ export const ProductCard = component$<ProductCardProps>(
 						size='sm'
 						class='inline-flex items-center justify-center font-medium text-base focus-visible:outline focus-visible:outline-offset rounded-md disabled:text-disabled-500 disabled:bg-disabled-300 disabled:shadow-none disabled:ring-0 disabled:cursor-not-allowed leading-5 text-sm py-1.5 px-3 gap-1.5 text-white shadow hover:shadow-md active:shadow bg-primary-700 hover:bg-primary-800 active:bg-primary-900 disabled:bg-disabled-300'
 					>
-						{t('addToCartShort')}
+						{t('addToCartShort@@Add')}
 						<div q:slot='prefix'>
 							<SfIconShoppingCart size='sm' class='w-5 h-5' />
 						</div>
