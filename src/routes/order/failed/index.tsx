e83c5b4c226e406b28509b0a00190bb0c86e6d70
gioboa <giorgiob.boa@gmail.com>
2023-08-01 08:59:44 +0200

    add order failed

---
+++ src/routes/order/failed/index.tsx
@@ -0,0 +1,48 @@
+import { component$ } from '@builder.io/qwik';
+import { Link } from '@builder.io/qwik-city';
+import { Image } from 'qwik-image';
+import { useTranslate } from 'qwik-speak';
+import { SfButton, SfIconArrowBack } from 'qwik-storefront-ui';
+
+export default component$(() => {
+	const t = useTranslate();
+	return (
+		<div data-testid='order-failed-page' class='px-4 md:px-0'>
+			<div class='border border-1 border-neutral-200 mt-10 mb-20 rounded p-4 md:p-6 flex flex-col items-center max-w-2xl mx-auto'>
+				<Image
+					loading='eager'
+					layout='constrained'
+					objectFit='fill'
+					width={200}
+					height={200}
+					alt={t('orderSuccessfulImageAlt')}
+					src={'/images/empty-category.svg'}
+				/>
+				<h1 class='mt-6 mb-1 text-2xl'>We are really sorry.</h1>
+				<span class='font-medium'>Your order has not been completed.</span>
+				<div class='border border-1 border-neutral-200 rounded bg-neutral-100 p-4 w-full my-4 text-sm'>
+					{t('orderErrorMessage')}
+				</div>
+				<SfButton
+					as={Link}
+					href='/checkout'
+					class='max-md:w-full'
+					variant='secondary'
+				>
+					<div q:slot='prefix'>
+						<SfIconArrowBack />
+					</div>
+					{t('backToCheckout')}
+				</SfButton>
+				<SfButton
+					as={Link}
+					href='/'
+					class='mt-4 max-md:w-full'
+					variant='tertiary'
+				>
+					{t('continueShopping')}
+				</SfButton>
+			</div>
+		</div>
+	);
+});
