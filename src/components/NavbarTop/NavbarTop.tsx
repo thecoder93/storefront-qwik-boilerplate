4428e7b0d10f42dfe2080e694075e68a09fdf4f5
gioboa <giorgiob.boa@gmail.com>
2023-08-01 21:55:02 +0200

    change logo

--- src/components/NavbarTop/NavbarTop.tsx
+++ src/components/NavbarTop/NavbarTop.tsx
@@ -1,19 +1,21 @@
-import { Slot, component$ } from '@builder.io/qwik';
+import { Slot, component$, useComputed$ } from '@builder.io/qwik';
 import { Link, useLocation } from '@builder.io/qwik-city';
-import { VsfLogo } from '../VsfLogo/VsfLogo';
+import { Image } from 'qwik-image';
 
 export const NavbarTop = component$(() => {
 	const location = useLocation();
-	const isCheckoutPath =
-		location.url.pathname.includes('cart') ||
-		location.url.pathname.includes('checkout') ||
-		location.url.pathname.includes('order');
+	const isCheckoutPathSig = useComputed$(
+		() =>
+			location.url.pathname.includes('cart') ||
+			location.url.pathname.includes('checkout') ||
+			location.url.pathname.includes('order')
+	);
 
 	return (
 		<header
 			class={[
 				'h-14 md:h-20 flex z-50 md:sticky md:-top-5 md:pt-2.5 md:shadow-md',
-				isCheckoutPath
+				isCheckoutPathSig.value
 					? 'bg-white text-[#02C652] border-b border-neutral-200'
 					: 'bg-primary-700 text-white',
 			]}
@@ -21,9 +23,21 @@ export const NavbarTop = component$(() => {
 		>
 			<div class='flex gap-[clamp(1rem,3vw,3rem)] items-center w-full md:h-[60px] max-w-screen-3xl py-6 px-4 md:px-6 lg:px-10 mx-auto sticky top-0'>
 				<Link href='/' aria-label='SF Homepage' class='h-6 md:h-7 -mt-1.5'>
-					<VsfLogo />
+					<Image
+						loading='eager'
+						layout='constrained'
+						width={240}
+						height={30}
+						class='w-full h-full mt-1'
+						src={
+							isCheckoutPathSig.value
+								? '/images/SFUI.png'
+								: '/images/SFUI_white.png'
+						}
+						alt='SFUI Logo'
+					/>
 				</Link>
-				{!isCheckoutPath && <Slot />}
+				{!isCheckoutPathSig.value && <Slot />}
 			</div>
 		</header>
 	);
