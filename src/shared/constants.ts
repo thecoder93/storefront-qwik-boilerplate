76391a7a590704a57bd13cccea1328c59e462b2f
gioboa <giorgiob.boa@gmail.com>
2023-08-01 21:13:04 +0200

    change names

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,7 +1,7 @@
 export const DEFAULT_METADATA_URL = 'storefront-qwik-boilerplate.vercel.app';
-export const DEFAULT_METADATA_TITLE = 'Qwik and Vue Storefront UI';
+export const DEFAULT_METADATA_TITLE = 'Qwik and Storefront UI';
 export const DEFAULT_METADATA_DESCRIPTION =
-	'A storefront starter kit built with Qwik and Vue Storefront UI';
+	'A storefront starter kit built with Qwik and Storefront UI';
 export const DEFAULT_METADATA_IMAGE =
 	'https://storefront-qwik-boilerplate.vercel.app/images/social-image.png';
 export const IMAGE_PLACHEHOLDER =
