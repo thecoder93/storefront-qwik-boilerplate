92bf85f92c32b7cb8c69ff77120eb5db89e6ace3
gioboa <giorgiob.boa@gmail.com>
2023-08-03 21:16:48 +0200

    add products endpoint

---
+++ src/routes/api/products/index.ts
@@ -0,0 +1,12 @@
+import type { RequestHandler } from '@builder.io/qwik-city';
+import { products } from '~/mocks';
+
+export const onGet: RequestHandler = async ({ query, json }) => {
+	const PAGE_SIZE = 10;
+	const page = parseInt(query.get('page') || '1', PAGE_SIZE);
+	const totalPages = Math.ceil(products.length / PAGE_SIZE);
+	json(200, {
+		products: products.slice(PAGE_SIZE * (page - 1), PAGE_SIZE * page),
+		totalPages,
+	});
+};
