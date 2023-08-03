74b9dd22ffd41077e6278df87ee12dbb37156b51
Gianluca La Manna <gianluca.lamanna@it.clara.net>
2023-08-03 19:42:37 +0200

    add SfButton on page product details

--- src/routes/product/[slug]/index.tsx
+++ src/routes/product/[slug]/index.tsx
@@ -1,11 +1,12 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useContext } from '@builder.io/qwik';
 import { routeLoader$, type DocumentHead } from '@builder.io/qwik-city';
 import { Image } from 'qwik-image';
-import { SfRating } from 'qwik-storefront-ui';
+import { useTranslate } from 'qwik-speak';
+import { SfButton, SfIconShoppingCart, SfRating } from 'qwik-storefront-ui';
 import { ProductNotFound } from '~/components/ProductNotFound/ProductNotFound';
 import { ProductSlider } from '~/components/ProductSlider/ProductSlider';
 import { useRandomProductsLoader } from '~/routes/layout';
-import { IMAGE_PLACHEHOLDER } from '~/shared/constants';
+import { ACTIONS_CONTEXT, IMAGE_PLACHEHOLDER } from '~/shared/constants';
 import { generateDocumentHead } from '~/shared/utils';
 import type { Product } from '~/types/product';
 
@@ -23,6 +24,9 @@ export const useProductLoader = routeLoader$(
 
 export default component$(() => {
 	const product = useProductLoader();
+	const t = useTranslate();
+	const actions = useContext(ACTIONS_CONTEXT);
+
 	if (!product.value) {
 		return <ProductNotFound />;
 	}
@@ -305,21 +309,17 @@ export default component$(() => {
 												</button>
 											</div>
 										</div>
-										<button
+										<SfButton
 											type='button'
+											size='sm'
 											class='inline-flex items-center justify-center font-medium text-base focus-visible:outline focus-visible:outline-offset rounded-md disabled:text-disabled-500 disabled:bg-disabled-300 disabled:shadow-none disabled:ring-0 disabled:cursor-not-allowed py-3 leading-6 px-6 gap-3 text-white shadow hover:shadow-md active:shadow bg-primary-700 hover:bg-primary-800 active:bg-primary-900 disabled:bg-disabled-300 flex-grow-[2] flex-shrink basis-auto whitespace-nowrap'
-											data-testid='button'
+											onClick$={() => actions.updateCartWithProduct(product.value.id, 1)}
 										>
-											<svg
-												viewBox='0 0 24 24'
-												data-testid='shopping-cart'
-												xmlns='http://www.w3.org/2000/svg'
-												class='inline-block fill-current w-5 h-5 undefined'
-											>
-												<path d='M7 22c-.55 0-1.02-.196-1.412-.587A1.926 1.926 0 0 1 5 20c0-.55.196-1.02.588-1.413A1.926 1.926 0 0 1 7 18c.55 0 1.02.196 1.412.587C8.804 18.98 9 19.45 9 20s-.196 1.02-.588 1.413A1.926 1.926 0 0 1 7 22Zm10 0c-.55 0-1.02-.196-1.412-.587A1.926 1.926 0 0 1 15 20c0-.55.196-1.02.588-1.413A1.926 1.926 0 0 1 17 18c.55 0 1.02.196 1.413.587.391.392.587.863.587 1.413s-.196 1.02-.587 1.413A1.926 1.926 0 0 1 17 22ZM6.15 6l2.4 5h7l2.75-5H6.15ZM7 17c-.75 0-1.317-.33-1.7-.988-.383-.658-.4-1.312-.05-1.962L6.6 11.6 3 4H1.975a.927.927 0 0 1-.7-.288A.99.99 0 0 1 1 3c0-.283.096-.52.288-.712A.968.968 0 0 1 2 2h1.625c.183 0 .358.05.525.15a.93.93 0 0 1 .375.425L5.2 4h14.75c.45 0 .758.167.925.5.167.333.158.683-.025 1.05l-3.55 6.4a2.034 2.034 0 0 1-.725.775A1.93 1.93 0 0 1 15.55 13H8.1L7 15h11.025c.283 0 .517.096.7.287.183.192.275.43.275.713s-.096.52-.288.712A.968.968 0 0 1 18 17H7Z'></path>
-											</svg>
-											Add to cart{' '}
-										</button>
+											{t('addToCart@@Add to cart')}
+											<div q: slot='prefix'>
+												<SfIconShoppingCart size='sm' class='w-5 h-5' />
+											</div>
+										</SfButton>
 									</div>
 									<div class='flex justify-center mt-4 gap-x-4'>
 										<button
