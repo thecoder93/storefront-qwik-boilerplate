eeeeab7dfcbb255339e01817150c2ce83827dcc9
Gianluca La Manna <gianluca.lamanna@it.clara.net>
2023-08-03 19:48:44 +0200

    add action add to cart into category product

--- src/routes/category/index.tsx
+++ src/routes/category/index.tsx
@@ -1,10 +1,15 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useContext } from '@builder.io/qwik';
 import { Image } from 'qwik-image';
-import { SfRating } from 'qwik-storefront-ui';
+import { useTranslate } from 'qwik-speak';
+import { SfButton, SfIconShoppingCart, SfRating } from 'qwik-storefront-ui';
+import { ACTIONS_CONTEXT } from '~/shared/constants';
 import { useRandomProductsLoader } from '../layout';
 
 export default component$(() => {
 	const randomProducts = useRandomProductsLoader();
+	const t = useTranslate();
+	const actions = useContext(ACTIONS_CONTEXT);
+
 	return (
 		<>
 			<div
@@ -775,21 +780,17 @@ export default component$(() => {
 													>
 														${product.price.value.amount}
 													</span>
-													<button
+													<SfButton
 														type='button'
+														size='sm'
 														class='inline-flex items-center justify-center font-medium text-base focus-visible:outline focus-visible:outline-offset rounded-md disabled:text-disabled-500 disabled:bg-disabled-300 disabled:shadow-none disabled:ring-0 disabled:cursor-not-allowed leading-5 text-sm py-1.5 px-3 gap-1.5 text-white shadow hover:shadow-md active:shadow bg-primary-700 hover:bg-primary-800 active:bg-primary-900 disabled:bg-disabled-300 px-2 py-2'
-														data-testid='button'
+														onClick$={() => actions.updateCartWithProduct(product.id, 1)}
 													>
-														<svg
-															viewBox='0 0 24 24'
-															data-testid='shopping-cart'
-															xmlns='http://www.w3.org/2000/svg'
-															class='inline-block fill-current w-5 h-5 undefined'
-														>
-															<path d='M7 22c-.55 0-1.02-.196-1.412-.587A1.926 1.926 0 0 1 5 20c0-.55.196-1.02.588-1.413A1.926 1.926 0 0 1 7 18c.55 0 1.02.196 1.412.587C8.804 18.98 9 19.45 9 20s-.196 1.02-.588 1.413A1.926 1.926 0 0 1 7 22Zm10 0c-.55 0-1.02-.196-1.412-.587A1.926 1.926 0 0 1 15 20c0-.55.196-1.02.588-1.413A1.926 1.926 0 0 1 17 18c.55 0 1.02.196 1.413.587.391.392.587.863.587 1.413s-.196 1.02-.587 1.413A1.926 1.926 0 0 1 17 22ZM6.15 6l2.4 5h7l2.75-5H6.15ZM7 17c-.75 0-1.317-.33-1.7-.988-.383-.658-.4-1.312-.05-1.962L6.6 11.6 3 4H1.975a.927.927 0 0 1-.7-.288A.99.99 0 0 1 1 3c0-.283.096-.52.288-.712A.968.968 0 0 1 2 2h1.625c.183 0 .358.05.525.15a.93.93 0 0 1 .375.425L5.2 4h14.75c.45 0 .758.167.925.5.167.333.158.683-.025 1.05l-3.55 6.4a2.034 2.034 0 0 1-.725.775A1.93 1.93 0 0 1 15.55 13H8.1L7 15h11.025c.283 0 .517.096.7.287.183.192.275.43.275.713s-.096.52-.288.712A.968.968 0 0 1 18 17H7Z'></path>
-														</svg>
-														Add
-													</button>
+														{t('addToCartShort@@Add')}
+														<div q: slot='prefix'>
+															<SfIconShoppingCart size='sm' class='w-5 h-5' />
+														</div>
+													</SfButton>
 												</div>
 											</div>
 										</div>
