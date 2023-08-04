8f2fc4417db49fa99f7a3925b5e839b9d4841ab8
gioboa <giorgiob.boa@gmail.com>
2023-08-04 22:48:23 +0200

    rename var

--- src/components/OrderSummary/OrderSummary.tsx
+++ src/components/OrderSummary/OrderSummary.tsx
@@ -6,7 +6,7 @@ import { Divider } from '../UI/Divider/Divider';
 export const OrderSummary = component$(() => {
 	const t = useTranslate();
 	const actions = useContext(ACTIONS_CONTEXT);
-	const totalQuantity = useComputed$(() => actions.getCartQuantity());
+	const cartQuantity = useComputed$(() => actions.getCartQuantity());
 	return (
 		<div
 			class='md:shadow-lg md:rounded-md md:border md:border-neutral-100 col-span-5 md:sticky md:top-20 h-fit'
@@ -20,7 +20,7 @@ export const OrderSummary = component$(() => {
 					class='typography-text-base font-semibold'
 					data-testid='total-in-cart'
 				>
-					({t('itemsInCart')} {totalQuantity})
+					({t('itemsInCart')} {cartQuantity})
 				</p>
 			</div>
 			<div class='px-4 pb-4 mt-3 md:px-6 md:pb-6 md:mt-0'>
