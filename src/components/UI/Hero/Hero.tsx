a40ed1e8a3d00517fe9cae87b77aaba5672c7805
gioboa <giorgiob.boa@gmail.com>
2023-08-07 20:57:32 +0200

    change Hero buttons

--- src/components/UI/Hero/Hero.tsx
+++ src/components/UI/Hero/Hero.tsx
@@ -1,5 +1,5 @@
 import { component$ } from '@builder.io/qwik';
-import { Link } from '@builder.io/qwik-city';
+import { useNavigate } from '@builder.io/qwik-city';
 import { Image } from 'qwik-image';
 import { SfButton } from 'qwik-storefront-ui';
 
@@ -25,6 +25,7 @@ export const Hero = component$<HeroProps>(
 		secondaryButtonLink,
 		secondaryButtonText,
 	}) => {
+		const navigate = useNavigate();
 		return (
 			<div class='relative min-h-[600px] mb-10'>
 				<div class='md:flex md:flex-row-reverse md:justify-center min-h-[600px] max-w-screen-3xl mx-auto'>
@@ -52,18 +53,20 @@ export const Hero = component$<HeroProps>(
 						<div class='flex flex-col md:flex-row gap-4 mt-6'>
 							<SfButton
 								size='lg'
-								as={Link}
-								href={primaryButtonLink}
 								class="class='inline-flex items-center justify-center font-medium text-base focus-visible:outline focus-visible:outline-offset rounded-md disabled:text-disabled-500 disabled:bg-disabled-300 disabled:shadow-none disabled:ring-0 disabled:cursor-not-allowed py-3 leading-6 px-6 gap-3 text-white shadow hover:shadow-md active:shadow bg-primary-700 hover:bg-primary-800 active:bg-primary-900 disabled:bg-disabled-300'"
+								onClick$={() => {
+									navigate(primaryButtonLink);
+								}}
 							>
 								{primaryButtonText}
 							</SfButton>
 							<SfButton
 								size='lg'
-								as={Link}
-								href={secondaryButtonLink}
 								variant='secondary'
 								class='ring-1 ring-primary-700'
+								onClick$={() => {
+									navigate(secondaryButtonLink);
+								}}
 							>
 								{secondaryButtonText}
 							</SfButton>
